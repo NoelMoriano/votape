b2f5a78cc72fd891d1ee462420f513b136e77da8
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-20 18:46:30 -0500

    add style

--- src/pages/candidate-ranked/index.js
+++ src/pages/candidate-ranked/index.js
@@ -1,5 +1,31 @@
 import React from "react";
+import styled, { css } from "styled-components";
+import { SectionRanked } from "../../components";
 
 export const CandidateRanked = () => {
-  return <div>candidate ranked</div>;
+  return (
+    <Container>
+      <h2>Seguimiento de Votaciones</h2>
+
+      <SectionRanked />
+      <SectionRanked />
+      <SectionRanked />
+      <SectionRanked />
+      <SectionRanked />
+      <SectionRanked />
+    </Container>
+  );
 };
+
+const Container = styled.section`
+  ${({ theme }) => css`
+    padding: 1em;
+    h2 {
+      padding-top: 0.5em;
+      padding-bottom: 1em;
+      text-align: center;
+      font-size: ${theme.font_sizes.x_large};
+      color: ${theme.colors.primary};
+    }
+  `}
+`;
