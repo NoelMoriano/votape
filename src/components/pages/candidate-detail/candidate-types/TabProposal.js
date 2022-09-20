e77ee0d1662fde08d78882c9e784de6bc74135ba
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-20 18:08:59 -0500

    update content

--- src/components/pages/candidate-detail/candidate-types/TabProposal.js
+++ src/components/pages/candidate-detail/candidate-types/TabProposal.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export const TabProposal = () => {
-  return <div>propuestas</div>;
+  return (
+    <div>
+      <h3>Propuestas</h3>
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aut
+        consectetur distinctio expedita fuga iste iusto sit totam! Accusamus,
+        illo.
+      </p>
+    </div>
+  );
 };
