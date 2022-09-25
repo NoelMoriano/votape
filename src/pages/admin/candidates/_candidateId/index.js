056896b099e52431d1335abd6a48604f505ab109
EduardXI <eduardo.salazar@agenciaservitec.com>
2022-09-25 18:59:19 -0500

    add form candidates

--- src/pages/admin/candidates/_candidateId/index.js
+++ src/pages/admin/candidates/_candidateId/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Row from "antd/lib/row";
 import Col from "antd/lib/col";
-import { Button, Form, Input } from "../../../../components/layout/admin/ui";
+import {
+  Button,
+  Form,
+  Input,
+  RadioGroup,
+} from "../../../../components/layout/admin/ui";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -11,7 +16,15 @@ import { Divider } from "antd";
 
 export const Candidate = () => {
   const schema = yup.object({
-    animeCode: yup.string().required(),
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+    chargeRequest: yup.string().required(),
+    placeOfPostulation: yup.string().required(),
+    dni: yup.number().required(),
+    country: yup.string().required(),
+    department: yup.string().required(),
+    province: yup.string().required(),
+    district: yup.string().required(),
   });
 
   const {
@@ -33,12 +46,12 @@ export const Candidate = () => {
         <Form onSubmit={handleSubmit(onSubmitSaveCandidate)}>
           <Col span={24}>
             <Controller
-              name="animeCode"
+              name="firstName"
               control={control}
               defaultValue=""
               render={({ field: { onChange, value, name } }) => (
                 <Input
-                  label="Ingrese código o url del anime"
+                  label="Ingrese Nombres"
                   size="large"
                   name={name}
                   value={value}
@@ -48,6 +61,177 @@ export const Candidate = () => {
                 />
               )}
             />
+          </Col>
+          <Col span={24}>
+            <Controller
+              name="lastName"
+              control={control}
+              defaultValue=""
+              render={({ field: { onChange, value, name } }) => (
+                <Input
+                  label="Ingrese Apellidos"
+                  size="large"
+                  name={name}
+                  value={value}
+                  onChange={onChange}
+                  error={error(name)}
+                  required={required(name)}
+                />
+              )}
+            />
+          </Col>
+          <Col span={24}>
+            <Controller
+              name="chargeRequest"
+              control={control}
+              defaultValue=""
+              render={({ field: { onChange, value, name } }) => (
+                <Input
+                  label="Cargo al que postula "
+                  size="large"
+                  name={name}
+                  value={value}
+                  onChange={onChange}
+                  error={error(name)}
+                  required={required(name)}
+                />
+              )}
+            />
+          </Col>
+          <Col span={24}>
+            <Controller
+              name="placeOfPostulation"
+              control={control}
+              defaultValue=""
+              render={({ field: { onChange, value, name } }) => (
+                <Input
+                  label="Lugar al que postula "
+                  size="large"
+                  name={name}
+                  value={value}
+                  onChange={onChange}
+                  error={error(name)}
+                  required={required(name)}
+                />
+              )}
+            />
+          </Col>
+          <Col span={24}>
+            <Controller
+              name="dni"
+              control={control}
+              defaultValue=""
+              render={({ field: { onChange, value, name } }) => (
+                <Input
+                  label="Dni "
+                  size="large"
+                  name={name}
+                  value={value}
+                  onChange={onChange}
+                  error={error(name)}
+                  required={required(name)}
+                />
+              )}
+            />
+          </Col>
+          <Col span={24}>
+            <Controller
+              name="sex"
+              control={control}
+              defaultValue=""
+              render={({ field: { onChange, value, name } }) => (
+                <RadioGroup
+                  label="Sex "
+                  size="large"
+                  name={name}
+                  value={value}
+                  onChange={onChange}
+                  error={error(name)}
+                  required={required(name)}
+                  options={[
+                    { label: "Masculino", value: "male" },
+                    { label: "Femenino", value: "female" },
+                  ]}
+                />
+              )}
+            />
+          </Col>
+          <Col span={24}>
+            <Controller
+              name="country"
+              control={control}
+              defaultValue=""
+              render={({ field: { onChange, value, name } }) => (
+                <Input
+                  label="País "
+                  size="large"
+                  name={name}
+                  value={value}
+                  onChange={onChange}
+                  error={error(name)}
+                  required={required(name)}
+                />
+              )}
+            />
+          </Col>
+          <Col span={24}>
+            <Controller
+              name="department"
+              control={control}
+              defaultValue=""
+              render={({ field: { onChange, value, name } }) => (
+                <Input
+                  label="Departamento "
+                  size="large"
+                  name={name}
+                  value={value}
+                  onChange={onChange}
+                  error={error(name)}
+                  required={required(name)}
+                />
+              )}
+            />
+          </Col>
+          <Col span={24}>
+            <Controller
+              name="province"
+              control={control}
+              defaultValue=""
+              render={({ field: { onChange, value, name } }) => (
+                <Input
+                  label="Provincia "
+                  size="large"
+                  name={name}
+                  value={value}
+                  onChange={onChange}
+                  error={error(name)}
+                  required={required(name)}
+                />
+              )}
+            />
+          </Col>
+          <Col span={24}>
+            <Controller
+              name="district"
+              control={control}
+              defaultValue=""
+              render={({ field: { onChange, value, name } }) => (
+                <Input
+                  label="Distrito "
+                  size="large"
+                  name={name}
+                  value={value}
+                  onChange={onChange}
+                  error={error(name)}
+                  required={required(name)}
+                />
+              )}
+            />
+          </Col>
+          <Col span={24}>
+            <Button type="primary" htmlType="submit">
+              Guardar
+            </Button>
           </Col>
         </Form>
       </Col>
