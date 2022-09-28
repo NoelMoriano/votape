70f46dc48cf1acc59812e0e82dc55601158c11c5
angelgt <angel.gamonal@agenciaservitec.com>
2022-09-27 19:31:23 -0500

    get proposals

--- src/pages/admin/candidates/_candidateId/proposals/index.js
+++ src/pages/admin/candidates/_candidateId/proposals/index.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Row from "antd/lib/row";
 import Col from "antd/lib/col";
 import { List, Skeleton } from "antd";
 import { Link } from "react-router-dom";
 import Avatar from "antd/lib/avatar/avatar";
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
+import { firestore, querySnapshotToArray } from "../../../../../firebase";
 
 export const Proposals = () => {
   const navigate = useNavigate();
+  const { candidateId } = useParams();
+  const [proposals, setProposals] = useState([]);
+
+  useEffect(() => {
+    fetchProposals();
+  }, []);
+
+  const fetchProposals = async () => {
+    const queryProposals = await firestore
+      .collection("proposals")
+      .where("candidateId", "==", candidateId)
+      .get();
+
+    const proposals_ = querySnapshotToArray(queryProposals);
+
+    console.log("proposal", proposals_);
+
+    setProposals(proposals_);
+  };
 
   const onNavigateTo = (url) => navigate(url);
   return (
@@ -18,13 +38,15 @@ export const Proposals = () => {
           loading={false}
           itemLayout="horizontal"
           loadMore={false}
-          dataSource={[
-            { name: "Hola mundo", description: "description description" },
-          ]}
-          renderItem={() => (
+          dataSource={proposals}
+          renderItem={(proposal) => (
             <List.Item
               actions={[
-                <Link to="/admin/candidates/new">Edit</Link>,
+                <Link
+                  to={`/admin/candidates/${candidateId}/proposals/${proposal}`}
+                >
+                  Edit
+                </Link>,
                 <Link to="/admin/candidates/new">Delete</Link>,
               ]}
             >
@@ -33,10 +55,9 @@ export const Proposals = () => {
                   avatar={
                     <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
                   }
-                  title={<a href="https://ant.design">Titulo de anime</a>}
-                  description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                  title={<Link to="https://ant.design">{proposal.title}</Link>}
+                  description={proposal.description}
                 />
-                <div>content</div>
               </Skeleton>
             </List.Item>
           )}
